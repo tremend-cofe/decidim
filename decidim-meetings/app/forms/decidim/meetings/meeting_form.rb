42003d7f659f9f0398765907c2f45a5c9c349d60
Andrea Orler <andrea@codegram.com>
2020-11-10 12:56:29 +0100

    refactor: use constants from model

--- decidim-meetings/app/forms/decidim/meetings/meeting_form.rb
+++ decidim-meetings/app/forms/decidim/meetings/meeting_form.rb
@@ -25,9 +25,6 @@ module Decidim
       attribute :available_slots, Integer, default: 0
       attribute :registration_terms, String
 
-      TYPE_OF_MEETING = %w(in_person online).freeze
-      REGISTRATION_TYPE = %w(registration_disabled on_this_platform on_different_platform).freeze
-
       validates :title, presence: true
       validates :description, presence: true
       validates :type_of_meeting, presence: true
@@ -115,7 +112,7 @@ module Decidim
       end
 
       def type_of_meeting_select
-        TYPE_OF_MEETING.map do |type|
+        Decidim::Meetings::Meeting::TYPE_OF_MEETING.map do |type|
           [
             I18n.t("type_of_meeting.#{type}", scope: "decidim.meetings"),
             type
@@ -132,7 +129,7 @@ module Decidim
       end
 
       def registration_type_select
-        REGISTRATION_TYPE.map do |type|
+        Decidim::Meetings::Meeting::REGISTRATION_TYPE.map do |type|
           [
             I18n.t("registration_type.#{type}", scope: "decidim.meetings"),
             type
