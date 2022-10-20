379b2b37b2ce296c1efdeef213683c0b90c62003
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-10-20 22:05:49 +0300

    JS linter

--- decidim-templates/app/packs/src/decidim/templates/admin/block_template_chooser.js
+++ decidim-templates/app/packs/src/decidim/templates/admin/block_template_chooser.js
@@ -2,7 +2,7 @@ $(() => {
   $("#block_template_chooser").change(function() {
     let dropDown =  $("#block_template_chooser");
     $.getJSON(dropDown.data("url"), {
-      id: dropDown.val(),
+      id: dropDown.val()
     }).done(function(data) {
       console.log(data);
       $("#block_user_justification").val(data.template);
