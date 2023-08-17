dca8f4bf05259a42bf8d4ddba898a9589b48be8d
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-08-17 08:51:55 +0300

    Refactor gitignore

--- decidim-generators/lib/decidim/generators/app_generator.rb
+++ decidim-generators/lib/decidim/generators/app_generator.rb
@@ -245,31 +245,6 @@ module Decidim
         CONFIG
       end
 
-      def modify_gitignore
-        return if options[:skip_git]
-
-        append_file ".gitignore", <<~GITIGNORE
-
-          # Ignore env configuration files
-          .env
-          .envrc
-          .rbenv-vars
-
-          # Ignore the files and folders generated through Webpack
-          /public/decidim-packs
-          /public/packs-test
-          /public/sw.js
-          /public/sw.js.map
-
-          # Ignore node modules
-          /node_modules
-        GITIGNORE
-      end
-
-      def add_ignore_tailwind_configuration
-        append_file ".gitignore", "\n\n# Ignore Tailwind configuration\ntailwind.config.js" unless options["skip_git"]
-      end
-
       def decidim_initializer
         copy_file "initializer.rb", "config/initializers/decidim.rb"
 
