82a5863fd644381f926817f5e9a8a5db858941ea
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-06-23 00:26:53 +0300

    Patch FooterSubHeroComponent

--- decidim-core/app/components/decidim/content_blocks/base_component.rb
+++ decidim-core/app/components/decidim/content_blocks/base_component.rb
@@ -12,6 +12,7 @@ module Decidim
       attr_reader :content_block
 
       delegate :settings, :images_container, :organization, to: :content_block
+      def current_organization_name = organization_name(organization)
     end
   end
 end
---
+++ decidim-core/spec/components/previews/decidim/content_blocks/footer_sub_hero_component_preview.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Decidim::ContentBlocks::FooterSubHeroComponentPreview < ViewComponent::Preview
+  def default
+    # example content block object
+   content_block =  Decidim::ContentBlock.new(
+      manifest_name: "footer_sub_hero",
+      scope_name: "homepage",
+      organization:
+    )
+
+    # usually rendered with
+    render(content_block.component)
+  end
+
+  private
+  def organization
+    Decidim::Organization.new(
+      users_registration_mode: "enabled",
+      name: { en: "My test organization" }
+    )
+  end
+end
