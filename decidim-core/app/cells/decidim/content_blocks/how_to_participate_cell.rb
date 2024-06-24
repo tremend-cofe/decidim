a97f6d9b890cff91abd80592d1f662cbca51b6c6
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-06-24 07:41:56 +0300

    Patch HowToParticipate Component

--- decidim-core/app/cells/decidim/content_blocks/how_to_participate_cell.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  module ContentBlocks
-    class HowToParticipateCell < Decidim::ViewModel
-    end
-  end
-end
--- decidim-core/spec/components/previews/decidim/content_blocks/how_to_participate_component_preview.rb
+++ decidim-core/spec/components/previews/decidim/content_blocks/how_to_participate_component_preview.rb
@@ -5,10 +5,17 @@ class Decidim::ContentBlocks::HowToParticipateComponentPreview < ViewComponent::
     # example content block object
     content_block = Decidim::ContentBlock.new(
       manifest_name: "how_to_participate",
-      scope_name: "homepage"
+      scope_name: "homepage",
+      organization:
     )
 
     # usually rendered with
     render(content_block.component)
+  end
+
+  private
+
+  def organization
+    Decidim::Organization.new( name: { en: "My example organization" } )
   end
 end
