932fc09154f89f1738698323bd175420b9ba0666
Màxim Colls <collsmaxim@gmail.com>
2024-06-28 12:34:37 +0200

    Improvements in the assignment of evaluators (#12994)
    * Add valuators multiselect in proposals page to assign multiple valuators in bulk
    
    * Implement unassign valuator
    
    * Allow valuators assign other valuators to proposal
    
    * Restore unrelated changes done by prettier in proposals.js
    
    * Fix erblint
    
    * Fix specs
    
    * Fix more specs
    
    * Hide assignment/unassignment panel when no proposals are selected
    
    * Revert "Allow valuators assign other valuators to proposal"
    
    This reverts commit da11fc390a34e011e1c80e2b3fa0f7682c107fb9.
    
    * Update permissions so valuators can assign other valuators when they are assigned to the same proposal
    
    * Fix assign/unassign actions panel UX to only show one at the time
    
    * Fix valuator being able to assign/unassign themselves in the proposals list
    
    * Update permissions to disallow valuators unassign themselves
    
    * Fix and remove specs
    
    * Remove spec
    
    * Hide Unassign from valuator action to valuators
    
    * Fix spec
    
    * Update copy
    
    * Add tests for multiple assignment / unassignment of evaluators
    
    * Update copy key

--- decidim-proposals/spec/system/admin/valuator_manages_proposals_spec.rb
+++ decidim-proposals/spec/system/admin/valuator_manages_proposals_spec.rb
@@ -41,26 +41,11 @@ describe "Valuator manages proposals" do
         page.first(".js-proposal-list-check").set(true)
       end
 
-      click_button "Actions"
-      click_button "Unassign from valuator"
-    end
-
-    it "can unassign themselves" do
-      within "#js-form-unassign-proposals-from-valuator" do
-        select user.name, from: :valuator_role_id
-        click_button(id: "js-submit-unassign-proposals-from-valuator")
-      end
-
-      expect(page).to have_content("Valuator unassigned from proposals successfully")
+      click_on "Actions"
     end
 
     it "cannot unassign others" do
-      within "#js-form-unassign-proposals-from-valuator" do
-        select another_user.name, from: :valuator_role_id
-        click_button(id: "js-submit-unassign-proposals-from-valuator")
-      end
-
-      expect(page).to have_content("You are not authorized to perform this action")
+      expect(page).to have_no_content("Unassign from valuator")
     end
   end
 
@@ -69,26 +54,6 @@ describe "Valuator manages proposals" do
       within find("tr", text: translated(assigned_proposal.title)) do
         click_link "Answer proposal"
       end
-    end
-
-    it "can only unassign themselves" do
-      within "#valuators" do
-        expect(page).to have_content(user.name)
-        expect(page).to have_content(another_user.name)
-
-        within find("li", text: another_user.name) do
-          expect(page).not_to have_selector("a.red-icon")
-        end
-
-        within find("li", text: user.name) do
-          expect(page).to have_selector("a.red-icon")
-          accept_confirm do
-            find("a.red-icon").click
-          end
-        end
-      end
-
-      expect(page).to have_content("successfully")
     end
 
     it "can leave proposal notes" do
